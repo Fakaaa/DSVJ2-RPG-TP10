b29b254fa9092b5c504564a943e6bf711277edf4
micaluzbenitez <83891619+micaluzbenitez@users.noreply.github.com>
2021-06-11 19:01:00 -0300

    Added loading screen scripts.

---
+++ DSVJ2-RPG-TP10/Assets/Scripts/LoaderScreen/UILoadNextScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LoaderManagerScript;
+using UILoadingScreenScript;
+
+namespace UILoadNextSceneScript
+{
+    public class UILoadNextScene : MonoBehaviour
+    {
+        public void LoadNextScene(string sceneName)
+        {
+            LoaderManager.Get().LoadScene(sceneName);
+            UILoadingScreen.Get().SetVisible(true);
+        }
+    }
+}
--- DSVJ2-RPG-TP10/Assets/Scripts/Managers/LoaderManager.cs
+++ DSVJ2-RPG-TP10/Assets/Scripts/Managers/LoaderManager.cs
@@ -1,13 +1,70 @@
-﻿using UnityEngine.SceneManagement;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using MonoBehaviourSingletonScript;
 
 namespace LoaderManagerScript
 {
     public class LoaderManager : MonoBehaviourSingleton<LoaderManager>
     {
+        public float loadingProgress;
+        public bool fakeLoad;
+        public float timeLoading;
+        public float minTimeToLoad = 2;
+
+        //public void LoadScene(string sceneName)
+        //{
+        //    SceneManager.LoadScene(sceneName);
+        //}
+
         public void LoadScene(string sceneName)
         {
-            SceneManager.LoadScene(sceneName);
+            if (fakeLoad) StartCoroutine(AsynchronousLoadWithFake(sceneName));
+            else StartCoroutine(AsynchronousLoad(sceneName));
+        }
+
+        IEnumerator AsynchronousLoad(string scene)
+        {
+            loadingProgress = 0;
+
+            yield return null;
+
+            AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+            ao.allowSceneActivation = false;
+
+            while (!ao.isDone)
+            {
+                loadingProgress = ao.progress + 0.1f;
+
+                if (ao.progress >= 0.9f)
+                    ao.allowSceneActivation = true;
+
+                yield return null;
+            }
+        }
+
+        IEnumerator AsynchronousLoadWithFake(string scene)
+        {
+            loadingProgress = 0;
+            timeLoading = 0;
+
+            yield return null;
+
+            AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+            ao.allowSceneActivation = false;
+
+            while (!ao.isDone)
+            {
+                timeLoading += Time.deltaTime;
+                loadingProgress = ao.progress + 0.1f;
+                loadingProgress = loadingProgress * timeLoading / minTimeToLoad;
+
+                if (loadingProgress >= 1)
+                    ao.allowSceneActivation = true;
+
+                yield return null;
+            }
         }
     }
 }
