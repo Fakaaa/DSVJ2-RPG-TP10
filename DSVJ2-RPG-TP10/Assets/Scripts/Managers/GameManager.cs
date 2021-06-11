1d8534afd80b798586f1ebf17e0e42403ccb0dbc
micaluzbenitez <83891619+micaluzbenitez@users.noreply.github.com>
2021-06-11 18:14:53 -0300

    Added managers.

---
+++ DSVJ2-RPG-TP10/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonoBehaviourSingletonScript;
+
+namespace GameManagerScript
+{
+    public class GameManager : MonoBehaviourSingleton<GameManager>
+    {
+
+    }
+}
