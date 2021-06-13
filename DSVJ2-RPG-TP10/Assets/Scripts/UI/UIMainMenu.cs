33ddf401eeb13f981522d5d6f2f486af2e36bf8e
FacundoPonce <mrfacu125@gmail.com>
2021-06-13 20:02:14 -0300

    Fixed the ranged bug.
    We need always set the coldown of the attack and some range to the attack.

--- DSVJ2-RPG-TP10/Assets/Scripts/UI/UIMainMenu.cs
+++ DSVJ2-RPG-TP10/Assets/Scripts/UI/UIMainMenu.cs
@@ -22,6 +22,9 @@ namespace UIMainMenuScript
 
         public void ExitGame()
         {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
             Application.Quit();
         }
     }
