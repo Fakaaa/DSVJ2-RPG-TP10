7343199cf3b02019cc063ed2e9fb0fd207f8a45c
micaluzbenitez <83891619+micaluzbenitez@users.noreply.github.com>
2021-06-13 20:12:21 -0300

    Added credits scene.

---
+++ DSVJ2-RPG-TP10/Assets/Scripts/UI/UICredits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LoaderManagerScript;
+using UILoadingScreenScript;
+
+namespace UICreditsScript
+{
+    public class UICredits : MonoBehaviour
+    {
+        public void LoadMenuScene()
+        {
+            LoaderManager.Get().LoadScene("MainMenu");
+            UILoadingScreen.Get().SetVisible(true);
+        }
+
+        public void ExitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+        }
+    }
+}
