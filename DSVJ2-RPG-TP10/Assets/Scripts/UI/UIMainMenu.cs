742fad39cea9cb324557e5331f6c5cc72fe3bcf3
FacundoPonce <mrfacu125@gmail.com>
2021-06-13 23:44:17 -0300

    Fixed bug with main menu.

--- DSVJ2-RPG-TP10/Assets/Scripts/UI/UIMainMenu.cs
+++ DSVJ2-RPG-TP10/Assets/Scripts/UI/UIMainMenu.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using LoaderManagerScript;
 using UILoadingScreenScript;
 
@@ -12,6 +10,7 @@ namespace UIMainMenuScript
         {
             LoaderManager.Get().LoadScene("Game"); 
             UILoadingScreen.Get().SetVisible(true);
+            gameObject.SetActive(false);
         }
 
         public void LoadCreditsScene()
